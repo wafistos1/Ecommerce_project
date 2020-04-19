208f8775afdbafa248f44a0209f5b656ba7dc1fe
wafi <mameri.wafi@gmail.com>
2020-04-19 18:44:20 +0100

    add forms.py and templates of accounts app

--- Mysite/accounts/urls.py
+++ Mysite/accounts/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path
-from accounts import views  
+from accounts import views
+from django.contrib.auth import views as auth_views
 from django.conf import settings
 from django.conf.urls.static import static
 
 # Mettre ca temporairement ( )
 urlpatterns = [
 path('', views.home, name='home'),
+path('accounts/login', views.login, name='login'),
+
 ]
--- Mysite/accounts/views.py
+++ Mysite/accounts/views.py
@@ -3,3 +3,5 @@ from django.shortcuts import render
 # Create your views here.
 def home(request):
     return render(request, 'base.html')
+def login(request):
+    return render(request, 'accounts/login.html')
