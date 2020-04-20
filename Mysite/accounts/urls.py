468b4234193a2b25cf1ec1729f832b98829020d6
wafi <mameri.wafi@gmail.com>
2020-04-20 19:47:02 +0100

    add new navbar and login modal

--- Mysite/accounts/urls.py
+++ Mysite/accounts/urls.py
@@ -7,7 +7,10 @@ from django.conf.urls.static import static
 # Mettre ca temporairement ( )
 urlpatterns = [
 path('', views.home, name='home'),
-path('accounts/login', views.login, name='login'),
+
+# path('login/', auth_views.LoginView.as_view(template_name='register/login.html'), name='login'),
+# path('logout/', auth_views.LogoutView.as_view(template_name='register/logout.html'), name='logout'),
+
 path('accounts/user', views.register, name='register'),
 
 ]
--- Mysite/accounts/views.py
+++ Mysite/accounts/views.py
@@ -10,10 +10,25 @@ def login(request):
 
 def register(request):
     print(request.POST)
-
-    user_form = UserRegisterForm()
-    adresse_form = adresseForm()
-    profile_form = profileForm()
+    if request.method == "POST":
+        user_form = UserRegisterForm(request.POST)
+        adresse_form = adresseForm(request.POST)
+        profile_form = profileForm(request.POST, request.FILES)
+        if user_form.is_valid() and adresse_form.is_valid() and profile_form.is_valid():
+            user = user_form.save()
+            adresse = adresse_form.save()
+            profile = profile_form.save(commit=False)
+            profile.user = user
+            profile.adresse = adresse
+            profile.save()
+            messages.add_message(
+                request, messages.SUCCESS, 'Profile inscrit avec succès veuillez vous connectez'
+                )
+            return redirect('home')
+    else:
+        user_form = UserRegisterForm()
+        adresse_form = adresseForm()
+        profile_form = profileForm()
     
 
     return render(request, 'accounts/user.html', {
@@ -21,3 +36,10 @@ def register(request):
         'adresse_form': adresse_form, 
         "profile_form": profile_form
         })
+
+    
+@login_required(login_url='login')
+def compte(request):
+    """ Display Details of User
+    """
+    return render(request, 'accounts/compte.html', locals())
