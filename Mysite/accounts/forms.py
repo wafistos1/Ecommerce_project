61b52acad0e3600dbf2eb8d773e1ef2e7fbc75f9
wafi <mameri.wafi@gmail.com>
2020-04-22 23:04:39 +0100

    add style.css to the userform registration

--- Mysite/accounts/forms.py
+++ Mysite/accounts/forms.py
@@ -8,14 +8,18 @@ from django.contrib.auth.forms import UserCreationForm
 class UserRegisterForm(UserCreationForm):
     """ Class form for user registration
     """
-    email = forms.EmailField()
-
+    
 
     def __init__(self, *args, **kwargs):
         """Hide help message for register user
         """
         super(UserCreationForm, self).__init__(*args, **kwargs)
         for fieldname in ['username', 'password1', 'password2']:
+            self.fields['username'].widget.attrs.update({'class': 'form-control'})
+            self.fields['first_name'].widget.attrs.update({'class': 'form-control'})
+            self.fields['email'].widget.attrs.update({'class': 'form-control'})
+            self.fields['password1'].widget.attrs.update({'class': 'form-control'})
+            self.fields['password2'].widget.attrs.update({'class': 'form-control'})
             self.fields[fieldname].help_text = None
 
     class Meta:
@@ -25,11 +29,23 @@ class UserRegisterForm(UserCreationForm):
         fields = ['username', 'email', 'password1', 'password2', 'first_name']
 
 class adresseForm(ModelForm):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['rue'].widget.attrs.update({'class': 'form-control'})
+        self.fields['ville'].widget.attrs.update({'class': 'form-control'})
+        self.fields['Zip'].widget.attrs.update({'class': 'form-control'})
+        self.fields['pays'].widget.attrs.update({'class': 'form-control'})
     class Meta:
         model = Adresse
-        fields = "__all__" 
+        fields = "__all__"
+        
 
 class profileForm(ModelForm):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+    
+        self.fields['avatar'].widget.attrs.update({'class': 'form-control'})
+        self.fields['telephone'].widget.attrs.update({'class': 'form-control'})
     class Meta:
         model = Profile
         fiels = ('avatar', 'telephone' )
