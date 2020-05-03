557645c6006db81ee8ee4938572ead50a0ca9e70
wafistos <mameri.wafi@gmail.com>
2020-05-03 13:37:20 +0100

    add uuid;pk url

--- Mysite/annonce/urls.py
+++ Mysite/annonce/urls.py
@@ -4,6 +4,6 @@ from .views import AnnonceDetailView, annonceListView
 
 urlpatterns = [
     path('annonce/add', views.add_annonce, name='add_annonce'),
-    path('annonce/detail', AnnonceDetailView.as_view(), name='annonce_detail'),
-    path('annonce/list', annonceListView.as_view(), name='annonce_detail'),
+    path('annonce/detail/<uuid:pk>', AnnonceDetailView.as_view(), name='annonce_detail'),
+    path('annonce/list', annonceListView.as_view(), name='annonce_list'),
     ]
