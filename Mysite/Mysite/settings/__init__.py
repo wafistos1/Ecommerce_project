143b1aaf2924bc95f95b9e738f212c21ca2754d1
wafistos1 <mameri.wafi@gmail.com>
2020-06-01 21:38:22 +0100

    add travis

--- Mysite/Mysite/settings/__init__.py
+++ Mysite/Mysite/settings/__init__.py
@@ -197,8 +197,8 @@ ACCOUNT_EMAIL_VERIFICATION = 'mandatory'
 ACCOUNT_SIGNUP_FORM_CLASS = 'accounts.forms.SignupForm'
 AUTH_USER_MODEL = 'accounts.Profile'
 # email console for test
-
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
 
 
     # email backend for google
