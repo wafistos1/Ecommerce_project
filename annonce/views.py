023b1588d1ad75fd11534b83555b1f5052482479
wafistos1 <mameri.wafi@gmail.com>
2020-06-14 23:02:04 +0100

    add test_func

--- annonce/views.py
+++ annonce/views.py
@@ -82,7 +82,6 @@ def add_annonce(request):
         formset = ImageFormSet(queryset=Image.objects.none())
     a_form = annonceFrom()
     formset = ImageFormSet(queryset=Image.objects.none())
-    capture_message(f"add annonce par {request.user.username}", level="error")
     return render(request, 'annonce/add.html', {
         "a_form": a_form,
         "formset": formset,
