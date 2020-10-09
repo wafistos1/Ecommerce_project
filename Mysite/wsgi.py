7cf572550e333f1dcf64c2a345696f8eac0d8c6f
Wafi <44195647+wafistos1@users.noreply.github.com>
2020-10-09 18:45:54 +0100

    Update wsgi.py

--- Mysite/wsgi.py
+++ Mysite/wsgi.py
@@ -8,7 +8,7 @@ https://docs.djangoproject.com/en/3.0/howto/deployment/wsgi/
 """
 
 import os
-
+import django
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Mysite.settings')
